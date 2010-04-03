c1b86a718a66665085821e0a93b0763a229973f0
dkindeev@mail.ru <dkindeev@mail.ru@d43461dc-dc74-11de-82ea-1f113a1b7651>
2010-04-03 09:42:45 +0000

    Добавлен дополнительный заголовок маркера, изменена эргономика

--- trunk/works/admin/views/yandexmapsmarker/tmpl/form.php
+++ trunk/works/admin/views/yandexmapsmarker/tmpl/form.php
@@ -12,38 +12,48 @@ echo YandexMapsRender::renderFormStyle();?>
 <div class="col50">
 <fieldset class="adminform">
 	<legend><?php echo JText::_('Basic Settings'); ?></legend>
-	<table class="admintable">		
-		<?php		
-		echo YandexMapsRender::renderFormInput('title', 'Title', '', $this->item->title);
-		echo YandexMapsRender::renderFormItemImageButton('latitude', 'Latitude', $this->item->latitude, 50, 50, $this->buttonmap, 'onChange="setPMGPSLatitude(this.value);"');
-		echo YandexMapsRender::renderFormInput('longitude', 'Longitude', '', $this->item->longitude, 50, 50, 'onChange="setPMGPSLongitude(this.value);"');
-		echo YandexMapsRender::renderFormInput('gpslatitude', 'GPS Latitude', '', $this->item->gpslatitude, '', '', 'readonly', 'background:#f0f0f0');
-		echo YandexMapsRender::renderFormInput('gpslongitude', 'GPS Longitude', '', $this->item->gpslongitude, '', '', 'readonly', 'background:#f0f0f0');
+	<table width="50%" class="admintable" style="float:left;">		
+		<?php	
+		echo YandexMapsRender::renderFormInput('title', 'Titlemarker', '', $this->item->title);	
+		echo YandexMapsRender::renderFormInput('titlem', 'Titlemarker_2', '', $this->item->titlem);
 		echo YandexMapsRender::renderFormItemSpecial('catid', 'Map', '', $this->lists['catid'] );
 		echo YandexMapsRender::renderFormItemSpecial('published', 'Published', '', $this->lists['published'] );
 		echo YandexMapsRender::renderFormItemSpecial('ordering', 'Ordering', '', $this->lists['ordering'] );
 		echo YandexMapsRender::renderFormItemSpecial('description', 'Description', '', $this->editor->display( 'description',  $this->item->description, '550', '300', '60', '20', array('pagebreak', 'readmore') ) );
+
+		?>
+       
+	</table>
+    <div style="float:right; width:30%;">
+ 
+<?php  
+jimport('joomla.html.pane');
+$pane =& JPane::getInstance('Sliders');
+echo $pane->startPane('myPane');
+{
+echo $pane->startPanel(JText::_('Nastroykamarkera'), 'panel1');
+
+		echo '<table class="paramlist admintable">';
+		echo YandexMapsRender::renderFormItemImageButton('latitude', 'Latitude', $this->item->latitude, 50, 50, $this->buttonmap, 'onChange="setPMGPSLatitude(this.value);"');
+		echo YandexMapsRender::renderFormInput('longitude', 'Longitude', '', $this->item->longitude, 50, 50, 'onChange="setPMGPSLongitude(this.value);"');
+		echo YandexMapsRender::renderFormInput('gpslatitude', 'GPS Latitude', '', $this->item->gpslatitude, '', '', 'readonly', 'background:#f0f0f0');
+		echo YandexMapsRender::renderFormInput('gpslongitude', 'GPS Longitude', '', $this->item->gpslongitude, '', '', 'readonly', 'background:#f0f0f0');
 		$option = array(0 => 'No', 1 => 'Yes');
 		echo YandexMapsRender::renderFormSelectBox('displaygps', 'Display GPS', '', $this->item->displaygps, $option);
-		?>
-	</table>	
+		echo "</table>";
+
+echo $pane->endPanel();
+echo $pane->startPanel(JText::_('Design Settings'), 'panel2');
+
+echo '<table>'.YandexMapsRender::renderFormItemSpecial('icon', 'Marker Icon', '', $this->iconcombo, '').'</table>';
+echo $pane->endPanel();
+}
+echo $pane->endPane();?>
+
+</div>
+	
 </fieldset>
 </div>
-
-<div class="col50">
-<fieldset class="adminform">
-	<legend><?php echo JText::_('Design Settings'); ?></legend>
-	<table class="admintable">		
-		<?php
-	echo YandexMapsRender::renderFormItemSpecial('icon', 'Marker Icon', '', $this->iconcombo, '');
-		?>
-	</table>
-   
-</fieldset>
-</div>
-
-
-
 <div class="clr"></div>
 
 <input type="hidden" name="option" value="com_yandexmaps" />
@@ -51,5 +61,3 @@ echo YandexMapsRender::renderFormStyle();?>
 <input type="hidden" name="task" value="" />
 <input type="hidden" name="controller" value="yandexmapsmarker" />
 </form>
-
-	
